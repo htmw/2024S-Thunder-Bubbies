4ab6e44a7278a126c5b3274d8c6206fd841dd8a0
Bhargava645 <bhargavachilukuri9@gmail.com>
2024-05-05 19:23:48 -0400

    Adding navigations to Results screen

--- code/client/App.js
+++ code/client/App.js
@@ -1,44 +1,65 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import LoginScreen from "./screens/LoginScreen";
 import TermsAndConditionsScreen from "./screens/TermsAndConditionsScreen";
 import SignInScreen from "./screens/SignInScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import MainScreen from "./screens/MainScreen";
+import ImagePreview from "./screens/ImagePreview";
+import ResultScreen from "./screens/ResultScreen";
+import FeedbackScreen from "./screens/FeedbackScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="TermsAndConditions"
-          component={TermsAndConditionsScreen}
-          options={{ title: "Terms and Conditions" }}
-        />
-        <Stack.Screen
-          name="SignIn"
-          component={SignInScreen}
-          options={{ title: "Sign In" }}
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUpScreen}
-          options={{ title: "Sign Up" }}
-        />
-        <Stack.Screen
-          name="Main"
-          component={MainScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="TermsAndConditions"
+            component={TermsAndConditionsScreen}
+            options={{ title: "Terms and Conditions" }}
+          />
+          <Stack.Screen
+            name="SignIn"
+            component={SignInScreen}
+            options={{ title: "Sign In" }}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUpScreen}
+            options={{ title: "Sign Up" }}
+          />
+          <Stack.Screen
+            name="Main"
+            component={MainScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ImagePreview"
+            component={ImagePreview}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ResultScreen"
+            component={ResultScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Feedback"
+            component={FeedbackScreen}
+            options={{ title: "Feedback" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
